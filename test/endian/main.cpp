72eb9fb85c25c0b12c5f53eb3a040a4bebfca26c
zYr <zyr_zyr_@sohu.com>
2019-04-02 10:53:26 +0800

    add host byte order test program

---
+++ test/endian/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdint.h>
+union Endian
+{
+    uint16_t s;
+    char c[sizeof(uint16_t)];
+};
+static Endian un = { 0x0102 };
+static bool HostIsBigEdian() {
+    if (un.c[0] == 1)
+        return true;
+    else
+        return false;
+}
+
+int main()
+{
+	return HostIsBigEdian();
+}
