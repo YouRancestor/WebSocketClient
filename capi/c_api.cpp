1a5562e8fcf95c5df5d14b8663a3ff6f9c7763fc
zYr <zyr_zyr_@sohu.com>
2019-07-13 12:45:14 +0800

    clean up c API code

--- capi/c_api.cpp
+++ capi/c_api.cpp
@@ -3,65 +3,59 @@
 using namespace ws;
 struct websocket_client_t : public WebSocketClientImplCurl
 {
-	websocket_client_t() :conn_cb(NULL), recv_cb(NULL), opaque(NULL) {}
-	virtual void OnConnect(ConnectResult result)override;
-	virtual void OnRecv(Message msg, bool fin) override;
+    websocket_client_t() :conn_cb(NULL), recv_cb(NULL), opaque(NULL) {}
+    virtual void OnConnect(ConnectResult result)override;
+    virtual void OnRecv(Message msg, bool fin) override;
 
-	websocket_connect_callback conn_cb;
-	websocket_receive_callback recv_cb;
-	void* opaque;
+    websocket_client_connect_callback conn_cb;
+    websocket_client_receive_callback recv_cb;
+    void* opaque;
 };
 
-websocket_client_t* websocket_create_client()
+websocket_client_t* websocket_client_create()
 {
-	return new websocket_client_t;
+    return new websocket_client_t;
 }
- void websocket_destroy_client(websocket_client_t* client)
+ void websocket_client_destroy(websocket_client_t* client)
 {
-	delete client;
-}
-
-
-void websocket_connect_server(websocket_client_t* client, const char* url)
-{
-	client->Connect(url);
+    delete client;
 }
 
-int websocket_send_sessage(websocket_client_t* client, websocket_message msg)
+void websocket_client_connect_server(websocket_client_t* client, const char* url)
 {
-	return client->Send(ws::Message((FrameType)msg.type, msg.data, msg.len));
+    client->Connect(url);
 }
 
-void websocket_close_connection(websocket_client_t* client)
+int websocket_client_send_sessage(websocket_client_t* client, websocket_message_t msg)
 {
-	client->Close();
+    return client->Send(ws::Message((FrameType)msg.type, msg.data, msg.len));
 }
 
-void websocket_set_callbacks(
-	websocket_client_t* client,
-	websocket_connect_callback conn_cb,
-	websocket_receive_callback recv_cb,
-	void* opaque)
+void websocket_client_set_callbacks(
+    websocket_client_t* client,
+    websocket_client_connect_callback conn_cb,
+    websocket_client_receive_callback recv_cb,
+    void* opaque)
 {
-	client->conn_cb = conn_cb;
-	client->recv_cb = recv_cb;
-	client->opaque = opaque;
+    client->conn_cb = conn_cb;
+    client->recv_cb = recv_cb;
+    client->opaque = opaque;
 }
 
 void websocket_client_t::OnConnect(ConnectResult result)
 {
-	if(this->conn_cb)
-		this->conn_cb((websocket_connect_result)result, this->opaque);
+    if(this->conn_cb)
+        this->conn_cb((websocket_client_connect_result_t)result, this->opaque);
 }
 
 void websocket_client_t::OnRecv(Message msg, bool fin)
 {
-	if (this->recv_cb)
-	{
-		websocket_message message;
-		message.type = (websocket_frame_type)msg.type;
-		message.data = msg.data;
-		message.len = msg.len;
-		this->recv_cb(message, fin, this->opaque);
-	}
+    if (this->recv_cb)
+    {
+        websocket_message_t message;
+        message.type = (websocket_frame_type_t)msg.type;
+        message.data = msg.data;
+        message.len = msg.len;
+        this->recv_cb(message, fin, this->opaque);
+    }
 }
--- include/websocket_client.h
+++ include/websocket_client.h
@@ -33,67 +33,104 @@ extern "C"
 {
 #endif // __cplusplus
 
+typedef enum websocket_frame_type_t
+{
+    // Non-control frame.
+    Continuation = 0x0,
+    Text = 0x1,
+    Binary = 0x2,
+    NonControlReserved1 = 0x3,
+    NonControlReserved2 = 0x4,
+    NonControlReserved3 = 0x5,
+    NonControlReserved4 = 0x6,
+    NonControlReserved5 = 0x7,
 
+    // Control frame.
+    Close = 0x8,
+    Ping = 0x9,
+    Pong = 0xA,
+    ControlReserved1 = 0xB,
+    ControlReserved2 = 0xC,
+    ControlReserved3 = 0xD,
+    ControlReserved4 = 0xE,
+    ControlReserved5 = 0xF,
+} websocket_frame_type_t;
 
-	enum websocket_frame_type
-	{
-		// Non-control frame.
-		Continuation = 0x0,
-		Text = 0x1,
-		Binary = 0x2,
-		NonControlReserved1 = 0x3,
-		NonControlReserved2 = 0x4,
-		NonControlReserved3 = 0x5,
-		NonControlReserved4 = 0x6,
-		NonControlReserved5 = 0x7,
+typedef struct websocket_message_t
+{
+    websocket_frame_type_t type;
+    const char* data;
+    int len; // size of data in bytes
+}websocket_message_t;
 
-		// Control frame.
-		Close = 0x8,
-		Ping = 0x9,
-		Pong = 0xA,
-		ControlReserved1 = 0xB,
-		ControlReserved2 = 0xC,
-		ControlReserved3 = 0xD,
-		ControlReserved4 = 0xE,
-		ControlReserved5 = 0xF,
+typedef enum websocket_client_connect_result_t
+{
+    Success = 0,
+    Timeout = 1,
+    Reject = 2
+} websocket_client_connect_result_t;
 
-	};
+typedef struct websocket_client_t websocket_client_t;
 
-	enum websocket_connect_result
-	{
-		Success = 0,
-		Timeout = 1,
-		Reject = 2
-	};
+typedef void (*websocket_client_connect_callback)(websocket_client_connect_result_t result, void* opaque);
 
-	typedef struct websocket_message
-	{
-		websocket_frame_type type;
-		const char* data;
-		int len; // size of data in bytes
-	}websocket_message;
+typedef void (*websocket_client_receive_callback)(websocket_message_t msg, int fin, void* opaque);
 
+/**
+ * @brief create a websocket client instance
+ * @return websocket client instance
+ * @sa @anchor websocket_client_create_with_custom_http_header @anchor websocket_client_destroy
+ */
+WEBSOCKET_CLIENT_API websocket_client_t* websocket_client_create();
 
-	typedef struct websocket_client_t websocket_client_t;
+/**
+ * @brief create a websocket client instance
+ * @param custom_header array for customize HTTP header, one array item represents one line in HTTP
+ * header, including the request line
+ * @param nlines @em custom_header array size
+ * @return  websocket client instance
+ * @sa @anchor websocket_client_create @anchor websocket_client_destroy
+ */
+WEBSOCKET_CLIENT_API websocket_client_t* websocket_client_create_with_custom_http_header(const char** custom_header, int nlines);
 
-	typedef void (*websocket_connect_callback)(websocket_connect_result result, void* opaque);
+/**
+ * @brief destroy the specific websocket client instance
+ * @param client websocket client instance created by @anchor websocket_client_create
+ * or @anchor websocket_client_create_with_custom_http_header
+ * @sa @anchor websocket_client_create @anchor websocket_client_create_with_custom_http_header
+ */
+WEBSOCKET_CLIENT_API void websocket_client_destroy(websocket_client_t* client);
 
-	typedef void (*websocket_receive_callback)(websocket_message msg, int fin, void* opaque);
+/**
+ * @brief connect to websocket server
+ * @param client websocket client instance
+ * @param url the websocket server's url to connect
+ * @note This function is non-blocking, it starts a thread to establish the connection and returns immediately. \n
+ * The @em url string will be saved as a copy, you can release it after this function returns. \n
+ * Set callbacks by calling @anchor websocket_client_set_callbacks before connect to server.
+ */
+WEBSOCKET_CLIENT_API void websocket_client_connect_server(websocket_client_t* client, const char* url);
 
-	WEBSOCKET_CLIENT_API websocket_client_t* websocket_create_client();
+/**
+ * @brief send message to server
+ * @param client websocket client instance
+ * @param msg the message to send
+ * @return the total number of bytes sent
+ * @note You should make sure the connection has established or the message will not be sent.
+ */
+WEBSOCKET_CLIENT_API int websocket_client_send_sessage(websocket_client_t* client, websocket_message_t msg);
 
-	WEBSOCKET_CLIENT_API void websocket_connect_server(websocket_client_t* client, const char* url);
-
-	WEBSOCKET_CLIENT_API int websocket_send_sessage(websocket_client_t* client, websocket_message msg);
-
-	WEBSOCKET_CLIENT_API void websocket_close_connection(websocket_client_t* client);
-
-	WEBSOCKET_CLIENT_API void websocket_set_callbacks( websocket_client_t* client,
-													   websocket_connect_callback conn_cb,
-													   websocket_receive_callback recv_cb,
-													   void* opaque );
-
-
+/**
+ * @brief websocket_client_set_callbacks
+ * @param client websocket client instance
+ * @param conn_cb callback to check connetion result
+ * @param recv_cb callback to receive message
+ * @param opaque private pointer for the above callbacks
+ */
+WEBSOCKET_CLIENT_API void websocket_client_set_callbacks(websocket_client_t* client,
+                                                         websocket_client_connect_callback conn_cb,
+                                                         websocket_client_receive_callback recv_cb,
+                                                         void* opaque );
 
 #ifdef __cplusplus
 }
